890b7fa2843f3ccb5a3c84cdcec908c702fcc2b9
osvaldo gratereaux <osvaldo.nypl@gmail.com>
2022-01-05 19:33:06 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+
+function writeCards(name,event){
+  let message = [];
+  for(let i = 0; i < name.length; i++){
+    message.push (`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+  }
+  return message;
+}
+
+function countDown (int) {
+  while (int >= 0){
+    console.log(int); 
+    int--;
+  }
+  return int;
+}
